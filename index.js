c29d8229ff34eca4cf591fda4c93402c0f503551
Andrew De Guzman <52567387+theadg@users.noreply.github.com>
2024-01-27 22:58:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-document.querySelectorAll('.form-group').forEach((form) => {
-    if (form.children.length === 5) {
-        const randomChildIndex = Math.floor(Math.random() * 5)
-        Array.from(form.children[0].children)[randomChildIndex].click()
-    }
-})
+document.querySelectorAll('.form-group').forEach(form => {
+  if (form.children.length === 5) {
+      const randomChildIndex = Math.floor(Math.random() * 5);
+     Array.from(form.children[randomChildIndex].children)[1].click();
+  }
+});
