d8f1264ff23e3dedfee99bb051658ccc5f606c6c
Renz Rackhold <rackhold@gmail.com>
2024-01-28 11:44:26 +0800

    Fix popup.js

--- Auto Evaluator Extension/popup.js
+++ Auto Evaluator Extension/popup.js
@@ -1,10 +1,21 @@
-const button = document.querySelector('#evaluate'); 
+document
+  .getElementById("evaluate")
+  .addEventListener("click", triggerEvaluation);
 
-button.addEventListener('click', function() {
-    document.querySelectorAll('.form-group').forEach(form => {
-        if (form.children.length === 5) {
+function triggerEvaluation() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0];
+
+    chrome.scripting.executeScript({
+      target: { tabId: activeTab.id },
+      function: () => {
+        document.querySelectorAll(".form-group").forEach((form) => {
+          if (form.children.length === 5) {
             const randomChildIndex = Math.floor(Math.random() * 5);
             Array.from(form.children[randomChildIndex].children)[1].click();
-        }
+          }
+        });
+      },
     });
-});
+  });
+}
