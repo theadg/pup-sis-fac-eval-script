dcc4efaa05771ea235b49e616a9e132742b37016
Bernard De Guzman <bernard.deguzman@technomancer.biz>
2024-01-28 13:30:12 +0800

    Add comments

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-document.querySelectorAll('.form-group').forEach(form => {
-  if (form.children.length === 5) {
-      const randomChildIndex = Math.floor(Math.random() * 5);
-     Array.from(form.children[randomChildIndex].children)[1].click();
-  }
-});
+// Select and loop through each .form-groups
+document.querySelectorAll('.form-group').forEach((form) => {
+    // Select only the only forms with the 5 choices
+    if (form.children.length === 5) {
+        // Generate random Index or set it to a number from 0-4
+        const randomChildIndex = Math.floor(Math.random() * 5)
+        // Click the label or input button
+        Array.from(form.children[randomChildIndex].children)[1].click()
+    }
+})
